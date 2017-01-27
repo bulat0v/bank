b6a085be3dfab4790103ecdf11a4892079a1daba
ONYX <0nyx@tut.by>
2017-01-27 10:29:14 +0300

    Ferst class added

---
+++ src/com/java/developer/team/Main.java
@@ -0,0 +1,10 @@
+package com.java.developer.team;
+
+/**
+ * Created by root on 27.01.2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Пишем здесь наш проект");
+    }
+}
