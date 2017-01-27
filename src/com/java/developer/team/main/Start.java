15e7af951f75acdeaf692c09cf4975a87cb8d55d
ONYX <0nyx@tut.by>
2017-01-27 11:32:14 +0300

    test

---
+++ src/com/java/developer/team/main/Start.java
@@ -0,0 +1,10 @@
+package com.java.developer.team.main;
+
+/**
+ * Created by root on 27.01.2017.
+ */
+public class Start {
+    public static void main(String[] args) {
+        System.out.println("Пишем наш калькулятор");
+    }
+}
