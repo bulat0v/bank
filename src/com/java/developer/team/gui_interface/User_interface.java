008a78f86328939907df41bd831275303b2591a0
ONYX <0nyx@tut.by>
2017-01-27 15:36:00 +0300

    user interface

--- src/com/java/developer/team/gui_interface/User_interface.java
+++ src/com/java/developer/team/gui_interface/User_interface.java
@@ -1,16 +1,17 @@
 package com.java.developer.team.gui_interface;
 
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * Created by root on 27.01.2017.
  */
 public class User_interface extends JFrame {
-    private JPanel panel;
+    private JPanel panel,p2,p3;
     private JButton b1,b2,b3,b4,b5,b6,b7,b8,b9,b0;
     private JButton bPlus,bMinus,bDivide,bEqually,bPercent;
     private JButton bBackspace,bCe,bC;
-    private JLabel label;
+    private JFormattedTextField ftf;
 
     public User_interface(){
         panel = new JPanel();
@@ -24,12 +25,18 @@ public class User_interface extends JFrame {
     }
 
     public void initComponents(){
-        label = new JLabel();
-        label.setBounds(5,5,290,30);
-        panel.add(label);
-        bBackspace = new JButton("Backspace");
-        bBackspace.setBounds(5,40,100,20);
-        panel.add(bBackspace);
+        BorderLayout borLay = new BorderLayout();
+        panel.setLayout(borLay);
+        ftf = new JFormattedTextField();
+        panel.add(ftf, BorderLayout.NORTH);
+        p2 = new JPanel();
+        b1 = new JButton("1");
+        p2.add(b1);
+        b2 = new JButton("2");
+        p2.add(b2);
+        p2.add(new JButton("3"));
+
+        panel.add(p2, BorderLayout.CENTER);
     }
 
     public void action(){
